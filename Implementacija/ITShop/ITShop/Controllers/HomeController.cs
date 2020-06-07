99eec8fccf2b6ba3905e6d946049b5504685896b
eseremet1 <eseremet1@etf.unsa.ba>
2020-06-07 07:46:50 +0200

    neke izmjene

--- Implementacija/ITShop/ITShop/Controllers/HomeController.cs
+++ Implementacija/ITShop/ITShop/Controllers/HomeController.cs
@@ -37,7 +37,20 @@ namespace ITShop.Controllers
         {
             return View();
         }
+        public IActionResult Kontakti()
+        {
+            return View();
+        }
 
+        public IActionResult NačinPlaćanja()
+        {
+            return View();
+        }
+
+        public IActionResult NačinDostave()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
